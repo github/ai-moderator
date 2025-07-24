24a6d76dc7957306e6c013f17eae32c3d22c440e
Sean Goedecke <sgoedecke@github.com>
2025-07-24 13:29:55 +1000

    more setup

---
+++ rollup.config.ts
@@ -0,0 +1,31 @@
+// See: https://rollupjs.org/introduction/
+import { builtinModules } from 'node:module'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+
+const config = {
+  input: 'src/index.ts',
+  output: {
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  external: [...builtinModules, /^node:/],
+  plugins: [
+    typescript(),
+    nodeResolve({
+      preferBuiltins: true,
+      browser: false,
+      exportConditions: ['node']
+    }),
+    commonjs({
+      include: /node_modules/
+    }),
+    json()
+  ]
+}
+
+export default config
